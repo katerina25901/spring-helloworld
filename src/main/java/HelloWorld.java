ff80a19b3318a4e3848b102f390498b374b0f274
U_M10PT <U_M10PT@HNBA105031.moscow.alfaintra.net>
2020-11-18 17:03:07 +0300

    spring_hello_world

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,27 @@
+import java.util.Objects;
+
+public class HelloWorld {
+ 
+    private String message;
+ 
+    public String getMessage() {
+        return message;
+    }
+ 
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        HelloWorld that = (HelloWorld) o;
+        return Objects.equals(message, that.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(message);
+    }
+}
